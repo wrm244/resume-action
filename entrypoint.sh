609edccb186f599151595165e5e84c111df2b1a3
RiverMountain <wrm244@outlook.com>
2023-10-09 19:30:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,18 +20,24 @@ hexo clean
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
+    mkdir public
+    cd ./public
+    staticrypt index.html -p 021212WhsWhs@@
+    cd ..
     hexo g -d
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
+    mkdir public
     cd ./public
     staticrypt index.html -p 021212WhsWhs@@
     cd ..
     hexo g -d
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    cd ./public
+    mkdir public
+    cd $(pwd)/public
     staticrypt index.html -p 021212WhsWhs@@
     cd ..
     hexo g -d -m "$INPUT_COMMIT_MSG"

ad1aae7c27d42ac7b26ed278c19c48dcf3b811f4
RiverMountain <wrm244@outlook.com>
2023-10-09 20:51:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
-staticryptpw="$INPUT_STPW"
 # install hexo env
 npm install hexo-cli -g
 npm install -g staticrypt
@@ -22,7 +21,7 @@ if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
     hexo g
     cd ./public
-    staticrypt index.html -p staticryptpw -d ./
+    staticrypt index.html -p "$INPUT_STPW" -d ./
     old_text='<p class="staticrypt-title">Protected Page</p>'
     new_text='<p class="staticrypt-title">欢迎登陆河山的简历</p>'
     sed -i "s|$old_text|$new_text|g" ./index.html
@@ -38,7 +37,7 @@ then
     old_text='Remember me'
     new_text='记住我'
     sed -i "s|$old_text|$new_text|g" ./index.html
-    staticrypt ./en/index.html -p staticryptpw -d ./en
+    staticrypt ./en/index.html -p "$INPUT_STPW" -d ./en
     cd ..
     hexo deploy
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
@@ -47,7 +46,7 @@ then
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
     hexo g
     cd ./public
-    staticrypt index.html -p staticryptpw -d ./
+    staticrypt index.html -p "$INPUT_STPW" -d ./
     old_text='<p class="staticrypt-title">Protected Page</p>'
     new_text='<p class="staticrypt-title">欢迎登陆河山的简历</p>'
     sed -i "s|$old_text|$new_text|g" ./index.html
@@ -63,14 +62,14 @@ then
     old_text='Remember me'
     new_text='记住我'
     sed -i "s|$old_text|$new_text|g" ./index.html
-    staticrypt ./en/index.html -p staticryptpw -d ./en
+    staticrypt ./en/index.html -p "$INPUT_STPW" -d ./en
     cd ..
     hexo deploy
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
     hexo g
     cd ./public
-    staticrypt index.html -p staticryptpw -d ./
+    staticrypt index.html -p "$INPUT_STPW" -d ./
     old_text='<p class="staticrypt-title">Protected Page</p>'
     new_text='<p class="staticrypt-title">欢迎登陆河山的简历</p>'
     sed -i "s|$old_text|$new_text|g" ./index.html
@@ -86,7 +85,7 @@ else
     old_text='Remember me'
     new_text='记住我'
     sed -i "s|$old_text|$new_text|g" ./index.html
-    staticrypt ./en/index.html -p staticryptpw -d ./en
+    staticrypt ./en/index.html -p "$INPUT_STPW" -d ./en
     cd ..
     hexo deploy -m "$INPUT_COMMIT_MSG"
 fi
